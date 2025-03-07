4e9250ed97976ff69fdee72d4ded9cb2c6b5235e
emilbedard <2347542@csfoy.ca>
2025-03-07 13:32:27 -0500

    Fixed? 2.0

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-base: "/appweb-trpr01/"
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/appweb-trpr01/",
   plugins: [vue()],
 })
